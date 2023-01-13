9b3d039f0c9af0a885e3fb8c14949ef74e2be429
Angelo Ilardi <angeloilardi@Angelos-MacBook-Air.local>
2023-01-13 19:40:43 +0000

    added timer function for the countdown

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -3,6 +3,9 @@ let quizIntro = document.querySelector("#start-screen");
 let startButton = document.querySelector("#start");
 let quizQuestion = document.querySelector("#question-title");
 let quizAnsers =  document.querySelector("#choices");
+let timerDisplay = document.querySelector("#time");
+
+let secondsLeft = 61;
 
 
 // create a function that displays a question and the 4 possible answers
@@ -18,9 +21,27 @@ function displayQuestion (i) {
 
 // add event listener for the start button
 startButton.addEventListener("click", function() {
+   
     quizIntro.setAttribute("class", "hide");
     questionContainer.setAttribute("class", "");
-    displayQuestion(i)
+    displayQuestion(0);
+    timer();
+
 }
 )
+
+// timer function
+
+function timer () {
+    let countdown = setInterval(function(){
+        secondsLeft--;
+        timerDisplay.textContent = secondsLeft;
+        if (secondsLeft === 0) {
+            clearInterval(countdown)
+        }
+    }, 1000);
+    
+}
+
+//add event listener to the answers buttons
 
