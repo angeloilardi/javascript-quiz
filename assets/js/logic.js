4505d3680fbf92a0b96145036ee8f7b16da812e9
Angelo Ilardi <angeloilardi@Angelos-MacBook-Air.local>
2023-01-14 17:52:10 +0000

    added function for when the game is over (out of questions or out of time)

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,16 +4,26 @@ let startButton = document.querySelector("#start");
 let quizQuestion = document.querySelector("#question-title");
 let quizAnwsers = document.querySelector("#choices");
 let timerDisplay = document.querySelector("#time");
+let feedback = document.getElementById("feedback");
+let resultsDisplay = document.getElementById("end-screen");
+let score = document.getElementById("final-score");
+let initials = document.getElementById("initials");
+let scoreSubmitButton = document.getElementById("submit");
+scoreSubmitButton.addEventListener("click", submitScore);
+
 let i = 0;
 
 let secondsLeft = 61;
 
 // add event listener for the start button
 startButton.addEventListener("click", function () {
-    console.log("hey");
+    // the intro section is hidden
     quizIntro.setAttribute("class", "hide");
+    //the questions section is unhidden
     questionContainer.setAttribute("class", "");
+    //the timer starts
     timer();
+    // the first questions is displayed
     displayQuestion();
 }
 )
@@ -37,13 +47,10 @@ function displayQuestion() {
     console.log(answerButtons);
     // adds and event listener to the buttons
     for (let k = 0; k < answerButtons.length; k++) {
-        // each time a button is clicked the function checks if the answer is correct oor not
+        // each time a button is clicked the function checks if the answer selected is correct or not
         answerButtons[k].addEventListener("click", function(){
-            checkAnswer(k)
-
+            checkAnswer(k);
         });
-        
-
     }
 
 
@@ -55,30 +62,57 @@ function timer() {
     let countdown = setInterval(function () {
         secondsLeft--;
         timerDisplay.textContent = secondsLeft;
-        if (secondsLeft === 0) {
-            clearInterval(countdown)
+        if (secondsLeft <= 0 || i > questions.length -1) {
+            clearInterval(countdown);
+            gameOver();
         }
     }, 1000);
 
-}
-
-//add event listener to the answers buttons
-
-function checkAnswer(selectedChoice) {
+};
 
 
-    if (selectedChoice === questions[i].correct) {
-        i++;
-        displayQuestion(i);
-    } else {
+// // function that checks if the answer is correct and if there are questions left or if the time is over
+function checkAnswer(selectedChoice){
+    if (selectedChoice !== questions[i].correct) {
+        // if the answer is wrong, the timer goeas down by 10 sec
         secondsLeft -= 10;
-        i++;
-        displayQuestion(i);
+        feedback.classList.remove("hide");
+        feedback.innerHTML = ("Wrong!");
+        fadeEffect ();
+        
+    } else {
+        feedback.classList.remove("hide");
+        feedback.innerHTML = ("Correct!");
+        fadeEffect ();
     }
+    ;
+    i++;
+    if (i > questions.length-1) {
+        gameOver();
+    } else displayQuestion();
+}
+
+
+// function that keeps the result on the screen for 500 ms
+function fadeEffect () {
+    feedback.classList.remove("hide");
+        feedback.innerHTML = ("Wrong!");
+    setTimeout(function(){
+        feedback.classList.add("hide")
+    }, 500);
 }
 
 
 
+//function that get executed when the game is over
+
+function gameOver(){
+    console.log("gameover");
+        questionContainer.setAttribute("class", "hide");
+    resultsDisplay.classList.remove("hide");
+    score.textContent = (secondsLeft);
+    
+}
 
 
 
