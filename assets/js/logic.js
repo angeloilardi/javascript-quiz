e4c5f31e6cfb98058c97b77b5fe266729272cb98
Angelo Ilardi <angeloilardi@Angelos-MacBook-Air.local>
2023-01-14 10:49:03 +0000

    added function that checks if the answer is correct

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,46 +2,83 @@ let questionContainer = document.querySelector("#questions");
 let quizIntro = document.querySelector("#start-screen");
 let startButton = document.querySelector("#start");
 let quizQuestion = document.querySelector("#question-title");
-let quizAnsers =  document.querySelector("#choices");
+let quizAnwsers = document.querySelector("#choices");
 let timerDisplay = document.querySelector("#time");
+let i = 0;
 
 let secondsLeft = 61;
 
-
-// create a function that displays a question and the 4 possible answers
-function displayQuestion (i) {
-    quizQuestion.innerHTML = questions[i].question;
-    for (let j =0; j < questions[i].answers.length; j++){
-        let answer = document.createElement("button");
-        answer.innerHTML = j+1 + ". " + questions[i].answers[j];
-        choices.appendChild(answer);        
-    }
-
-}
-
 // add event listener for the start button
-startButton.addEventListener("click", function() {
-   
+startButton.addEventListener("click", function () {
+    console.log("hey");
     quizIntro.setAttribute("class", "hide");
     questionContainer.setAttribute("class", "");
-    displayQuestion(0);
     timer();
-
+    displayQuestion();
 }
 )
 
+
+// create a function that displays a question and the 4 possible answers
+function displayQuestion() {
+    // displays the title of the question
+    quizQuestion.innerHTML = questions[i].question;
+    // resets the choices buttons
+    choices.innerHTML = "";
+    // creates a button for each choice
+    for (let j = 0; j < questions[i].answers.length; j++) {
+        let answer = document.createElement("button");
+        answer.setAttribute("class", "answer");
+        answer.innerHTML = j + 1 + ". " + questions[i].answers[j];
+        choices.appendChild(answer);
+    }
+    // targets all the newly created buttons with a class of answer
+    let answerButtons = document.getElementsByClassName("answer")
+    console.log(answerButtons);
+    // adds and event listener to the buttons
+    for (let k = 0; k < answerButtons.length; k++) {
+        // each time a button is clicked the function checks if the answer is correct oor not
+        answerButtons[k].addEventListener("click", function(){
+            checkAnswer(k)
+
+        });
+        
+
+    }
+
+
+}
+
 // timer function
 
-function timer () {
-    let countdown = setInterval(function(){
+function timer() {
+    let countdown = setInterval(function () {
         secondsLeft--;
         timerDisplay.textContent = secondsLeft;
         if (secondsLeft === 0) {
             clearInterval(countdown)
         }
     }, 1000);
-    
+
 }
 
 //add event listener to the answers buttons
+
+function checkAnswer(selectedChoice) {
+
+
+    if (selectedChoice === questions[i].correct) {
+        i++;
+        displayQuestion(i);
+    } else {
+        secondsLeft -= 10;
+        i++;
+        displayQuestion(i);
+    }
+}
+
+
+
+
+
 
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -2,38 +2,26 @@ let questions = [
     {
         question: "What does DOM stand for?",
         answers: ["Domain Object Model", "Document Object Module", "Debugging Object Module", "Document Object Model"],
-        correct: function() {return this.answers[3]}
+        correct: 3
     },
     {
         question: "Using the push() method in an array",
-        answer1: "adds an element at the end of the array",
-        answer2: "adds an element at the start of the array",
-        answer3: "removes an element at the start of the array",
-        answer4: "removes an element at the end of the array",
-        correct: function() {return this.answer1}
+        answers: ["adds an element at the end of the array", "adds an element at the end of the array", "removes an element at the start of the array", "removes an element at the end of the array"],
+        correct: 0
     },
     {
         question: "Which one of the following is not a primitive data type?",
-        answer1: "letter",
-        answer2: "boolean",
-        answer3: "number",
-        answer4: "symbol",
-        correct: function() {return this.answer1}
+        answers: ["letter","boolean", "number", "symbol"],
+        correct: 0
     },
     {
         question: "What iterable method executes a callback on every item and returns undefined?",
-        answer1: "map()",
-        answer2: "every()",
-        answer3: "forEach()",
-        answer4: "reduce()",
-        correct: function() {return this.answer3}
+        answers: ["map()","every()","forEach()","reduce()"],
+        correct: 2
     },
     {
         question: "JavaScript has an in-built function JSON.parse() that:",
-        answer1: "converts JSON strings into JavaScript objects",
-        answer2: "converting an object into a JSON string:",
-        answer3: "converts a string into a number",
-        answer4: "converts a number into a string",
-        correct: function() {return this.answer1}
+        answers: ["converts JSON strings into JavaScript objects","converting an object into a JSON string:", "converts a string into a number","converts a number into a string"],
+        correct: 0
     }
 ]
