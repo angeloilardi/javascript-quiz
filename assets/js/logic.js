bcb4a294443240bb059ecd4d41952542850a9cca
Angelo Ilardi <angeloilardi@Angelos-MacBook-Air.local>
2023-01-15 08:40:02 +0000

    applied sound effects play to the quiz

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,4 @@
+// created a target for all the elements in the DOM that need to be manipulated
 let questionContainer = document.querySelector("#questions");
 let quizIntro = document.querySelector("#start-screen");
 let startButton = document.querySelector("#start");
@@ -12,22 +13,43 @@ let scoreSubmitButton = document.getElementById("submit");
 let initialsInput = document.getElementById("initials");
 scoreSubmitButton.addEventListener("click", submitScore);
 
+
+// variables for sfx files
+let correctSound = new Audio("./assets/sfx/correct.wav");
+let incorrectSound = new Audio("./assets/sfx/incorrect.wav");
+
+// a global index that keeps track of the question currently in use
 let i = 0;
 
-let secondsLeft = 61;
+// the countdown starting point in seconds
+let secondsLeft = 60;
 
-// add event listener for the start button
+// adds an event listener for the start button
 startButton.addEventListener("click", function () {
-    // the intro section is hidden
-    quizIntro.setAttribute("class", "hide");
-    //the questions section is unhidden
-    questionContainer.setAttribute("class", "");
+    // the intro section is hidden by adding the hide class
+    quizIntro.classList.add("hide");
+    //the questions section is unhidden by removing the hide class
+    questionContainer.classList.remove("hide");
     //the timer starts
     timer();
     // the first questions is displayed
     displayQuestion();
 }
 )
+
+// timer function
+
+function timer() {
+    let countdown = setInterval(function () {
+        secondsLeft--;
+        timerDisplay.textContent = secondsLeft;
+        if (secondsLeft <= 0 || i > questions.length - 1) {
+            clearInterval(countdown);
+            gameOver();
+        }
+    }, 1000);
+
+};
 
 
 // create a function that displays a question and the 4 possible answers
@@ -49,7 +71,7 @@ function displayQuestion() {
     // adds and event listener to the buttons
     for (let k = 0; k < answerButtons.length; k++) {
         // each time a button is clicked the function checks if the answer selected is correct or not
-        answerButtons[k].addEventListener("click", function(){
+        answerButtons[k].addEventListener("click", function () {
             checkAnswer(k);
         });
     }
@@ -57,48 +79,36 @@ function displayQuestion() {
 
 }
 
-// timer function
 
-function timer() {
-    let countdown = setInterval(function () {
-        secondsLeft--;
-        timerDisplay.textContent = secondsLeft;
-        if (secondsLeft <= 0 || i > questions.length -1) {
-            clearInterval(countdown);
-            gameOver();
-        }
-    }, 1000);
-
-};
-
-
-// // function that checks if the answer is correct and if there are questions left or if the time is over
-function checkAnswer(selectedChoice){
+// function that checks if the answer is correct and if there are questions left 
+function checkAnswer(selectedChoice) {
     if (selectedChoice !== questions[i].correct) {
         // if the answer is wrong, the timer goeas down by 10 sec
         secondsLeft -= 10;
         feedback.classList.remove("hide");
         feedback.innerHTML = ("Wrong!");
-        fadeEffect ();
-        
+        fadeEffect();
+        incorrectSound.play();
+
     } else {
         feedback.classList.remove("hide");
         feedback.innerHTML = ("Correct!");
-        fadeEffect ();
+        fadeEffect();
+        correctSound.play();
     }
     ;
     i++;
-    if (i > questions.length-1) {
+    if (i > questions.length - 1) {
         gameOver();
     } else displayQuestion();
 }
 
 
 // function that keeps the result on the screen for 500 ms
-function fadeEffect () {
+function fadeEffect() {
     feedback.classList.remove("hide");
-        feedback.innerHTML = ("Wrong!");
-    setTimeout(function(){
+    feedback.innerHTML = ("Wrong!");
+    setTimeout(function () {
         feedback.classList.add("hide")
     }, 500);
 }
@@ -107,26 +117,36 @@ function fadeEffect () {
 
 //function that get executed when the game is over
 
-function gameOver(){
+function gameOver() {
     console.log("gameover");
-        questionContainer.setAttribute("class", "hide");
+    questionContainer.setAttribute("class", "hide");
     resultsDisplay.classList.remove("hide");
-    score.textContent = (secondsLeft);
-    
+    if (secondsLeft < 0) {
+        score.textContent = 0;
+    } else score.textContent = (secondsLeft);
+
 }
 
 // function to submit the initials and the score
-
+scoreSubmitButton.addEventListener("click", submitScore);
 function submitScore(event) {
+    //prevent deafult is used for the submit button
     event.preventDefault();
+    // JSON parse is used to turn the object stored in the local storage into a JavaScript object
     let scoreList = JSON.parse(localStorage.getItem("updatedList"));
+    // if the object is empty, we set scoreList to an empty array to start with
     if (scoreList === null) scoreList = [];
+    // a new object with the last score is created
     let newSubmittedScore = {
         initials: initialsInput.value,
-        score: secondsLeft
+        score: score.textContent
     }
+    // the new score is saved in the local storage
     localStorage.setItem("score", JSON.stringify(newSubmittedScore));
+    // the new score is added to the existing array
     scoreList.push(newSubmittedScore);
+    // we update the existing object with the update array
     localStorage.setItem("updatedList", JSON.stringify(scoreList));
+    // the highscores.html file replaces index.html
     window.open("highscores.html", ("_self"));
 }
--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,9 +1,12 @@
 
+//targets the ordered list with a class of highscores
 let scores = document.getElementById("highscores");
+// the object with all the saved scores stored locally is parsed into a JS object
 let scoreList = JSON.parse(localStorage.getItem("updatedList"));
+//targets the 'clear highscores' button
 let clearScoresButton = document.getElementById("clear");
 
-// loops that create a li element for each saved score
+// loop that creates a li element for each saved score
 for (i = 0; i < scoreList.length; i++){
     let newScore = document.createElement("li");
     newScore.textContent= `${scoreList[i].initials} - ${scoreList[i].score}`;
