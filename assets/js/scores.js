abf3813457de4a6977f80b4c20a4a51fabe60bdd
Angelo Ilardi <angeloilardi@Angelos-MacBook-Air.local>
2023-01-15 21:37:51 +0000

    added funct ion to sort the scores in a descending order

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -3,8 +3,17 @@
 let scores = document.getElementById("highscores");
 // the object with all the saved scores stored locally is parsed into a JS object
 let scoreList = JSON.parse(localStorage.getItem("updatedList"));
+console.log(scoreList);
 //targets the 'clear highscores' button
 let clearScoresButton = document.getElementById("clear");
+
+//the scores are sorted in descending order
+scoreList.sort(function(a,b){
+    return b.score - a.score;
+}
+);
+
+console.log(scoreList);
 
 // loop that creates a li element for each saved score
 for (i = 0; i < scoreList.length; i++){
